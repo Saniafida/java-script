3ff49695bc19c4fdb1ce141dbdd2781e88b74495
sania fida <saniafida261@gmail.com>
2024-09-19 21:46:15 +0500

    operations

---
+++ chapter1/04_conversionOperation.js
@@ -0,0 +1,19 @@
+let value = 3
+let negvalue = -value
+
+//console.log(negValue);
+
+console.log(2+3);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/4);
+console.log(2%8);
+
+
+let str1 = "hello"
+
+
+
+
+
