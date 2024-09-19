74a11dbaee953bcd8e7796ba7bc00cc55f0e2caa
sania fida <saniafida261@gmail.com>
2024-09-19 11:20:14 +0500

    01_variables

---
+++ chapter1/01_variables.js
@@ -0,0 +1,20 @@
+const account_Id = 144553
+let accountEmail = "saniafida261@gmail.com"
+var accountpassword = "12345"
+accountCity = "pak"
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "Danyor"
+let accountstate;
+
+console.log(account_Id);
+
+// Use `const` when the value shouldn't change.
+// Use `let` when the value will change.
+// Avoid `var` because it can cause issues with block and function scope.
+
+
+console.table([account_Id, accountEmail, accountpassword, accountCity, accountstate])
