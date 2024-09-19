11213fcae56ab46765dd3b6e2603d2ec974fb157
sania fida <saniafida261@gmail.com>
2024-09-19 11:23:49 +0500

    datatypes

---
+++ chapter1/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert(2+2)//we are using nodejs, not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+    let name = "sania"
+    let age = 30
+    let isloggedin = false
+    let state;
+
+    //number => 2 to powe 54
+    // bigint
+    //string => true/false
+    //null => standalong value
+    //undefined =>
+    //symbol=>unique
+
+    //object
+
+console.log(typeof "sania");
