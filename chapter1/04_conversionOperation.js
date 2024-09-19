da034e918cad62fe673dc83ecec8dd3d6a11b24b
sania fida <saniafida261@gmail.com>
2024-09-20 01:02:46 +0500

    conversionOperation

--- chapter1/04_conversionOperation.js
+++ chapter1/04_conversionOperation.js
@@ -12,6 +12,25 @@ console.log(2%8);
 
 
 let str1 = "hello"
+let str2 = " sanii"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+1+"2");
+console.log("1"+"1"+2+2);
+console.log(1+1+"2"+"2");
+
+console.log(+true); 
+console.log(+"") // these both are correct but avoide to use this
+
+
+
+let gameCounter = 10
+gameCounter++
+console.log(gameCounter); // read about post prefix and pre prefix
 
 
 
